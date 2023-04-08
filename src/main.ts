cb82d0df159f2c4ff4303e12fdd0d3d3e746b35a
chakkyy <cmramirez1996@gmail.com>
2023-04-07 21:00:18 -0300

    chore: modify prettier settings and formatting the project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
 
-import './styles/main.scss'
+import './styles/main.scss';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia());
 
-app.mount('#app')
+app.mount('#app');
